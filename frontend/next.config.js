720ad791c0511a390fe559cae586e3cd036efdfd
hongbo.mo <hongbo.mo@upai.com>
2026-02-04 12:08:14 +0800

    fix front bug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
   async redirects() {
     return [
       {
